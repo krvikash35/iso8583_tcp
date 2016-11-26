8ad51cb0124d3ece93db2f2d23199584a232b605
vikash <krvikash35@gmail.com>
2016-11-26 11:44:57 -0800

    updated

---
+++ client/field_config.js
@@ -0,0 +1,912 @@
+fconfig = {
+  f1 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f2 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f3 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f4 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f5 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f6 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f7 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f8 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f9 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f10 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f11 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f12 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f13 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f14 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f15 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f16 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f17 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f18 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f19 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f20 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f21 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f22 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f23 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f24 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f25 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f26 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f27 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f28 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f29 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f30 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f31 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f32 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f33 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f34 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f35 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f36 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f37 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f38 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f39 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f40 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f41 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f42 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f43 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f44 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f45 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f46 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f47 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f48 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f49 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f50 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f51 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f52 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f53 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f54 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f55 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f56 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f57 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f58 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f59 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f60 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f61 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f62 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f63 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f64 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f65 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f66 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f67 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f68 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f69 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f70 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f71 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f72 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f73 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f74 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f75 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f76 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f77 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f78 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f78 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f79 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f80 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f81 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f82 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f83 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f84 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f85 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f86 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f87 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f88 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f88 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f89 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f90 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f91 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f92 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f93 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f94 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f95 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f96 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f97 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f98 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f99 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f100 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f101 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f102 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f103 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f104 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f105 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f106 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f107 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f108 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f109 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f110 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f111 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f112 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f113 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f114 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f115 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f116 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f117 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f118 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f119 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f120 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f121 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f122 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f123 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f124 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f125 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f126 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f127 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  },
+  f128 = {
+    desc = ''
+    max_len = '',
+    byte_len = '',
+    value = '',
+    type = ''
+  }
+}
