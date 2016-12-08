c0ea66ee241c6e6babe351cf1c2b390bec42380a
vikash <krvikash35@gmail.com>
2016-12-08 10:01:58 -0800

    updated

--- server/app.js
+++ server/app.js
@@ -3,39 +3,17 @@ var prop = require('./prop')
 
 var HOST = prop.host;
 var PORT = prop.port;
-
-// Create a server instance, and chain the listen function to it
-// The function passed to net.createServer() becomes the event handler for the 'connection' event
-// The sock object the callback function receives UNIQUE for each connection
 net.createServer(function(sock) {
-
-    // We have a connection - a socket object is assigned to the connection automatically
-    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-
-    // Add a 'data' event handler to this instance of socket
+    console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
     sock.on('data', function(data) {
-      var buffer_data = Buffer.from(data);
-      console.log("BYTE RECEVIED: %d",buffer_data.length);
-      console.log("readInt8(0): ", buffer_data.readInt8(0));
-      console.log("readInt8(1): ", buffer_data.readInt8(1));
-      console.log("readInt16BE(0): ", buffer_data.readInt16BE(0));
-      console.log("readInt16LE(0): ", buffer_data.readInt16LE(0));
-      var concated = ""
-      for(var i=0; i<buffer_data.length; i++){
-        concated = concated + " : " +buffer_data[i].toString()
-      }
-      console.log(concated);
-      console.log(buffer_data.toString('ascii'));
-
+        var buffer_data = Buffer.from(data);
+        console.log("BYTE RECEVIED: %d", buffer_data.length);
+        console.log(buffer_data.toString());
         sock.write(data);
-
     });
-
-    // Add a 'close' event handler to this instance of socket
     sock.on('close', function(data) {
-        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+        console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
     });
 
 }).listen(PORT, HOST);
-
-console.log('Server listening on ' + HOST +':'+ PORT);
+console.log('Server listening on ' + HOST + ':' + PORT);
---
+++ web/server/app.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
