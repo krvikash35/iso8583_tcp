ff7024d84be8a548472c6537359b5e5eabe5bc6d
vikash <krvikash35@gmail.com>
2016-12-12 12:49:30 -0800

    updated

--- client/data/data.js
+++ client/data/data.js
@@ -1,29 +1,23 @@
 /* pass fields data in below format: sample
-* give below is sample important fields
+ give below is sample important fields
+fvalues = {
+  f0: '1200',
+  f2: '23'
+}
 */
 
 fvalues = {
   f0: '1200',
   f2: '23',
-  // f3: '310000',
-  // f4: '0000000000000000',
-  // f11: '45353',
-  // f12: '20161021101010',
-  // f17: '20161021',
-  // f24: '200',
-  // f32: '00000004535',
-  // f49: 'IN',
-  // f102: '024        10100   SBRUP-015',
-  // f123: 'CMN'
-  f127: ' ',
-  subfield:{
-    f127: {
-      f2: 'vikash'
-    }
-  }
+  f3: '310000',
+  f4: '0000000000000000',
+  f11: '45353',
+  f12: '20161021101010',
+  f17: '20161021',
+  f24: '200',
+  f32: '00000004535',
+  f49: 'IN',
+  f102: '024        10100   SBRUP-015',
+  f123: 'CMN'
 }
-
-//“SAF<STAN_OF_REQUEST_TO_BE_ARCHIVED><COMMAND><TRAN_DATE_TIME><DCC_ID><CUST_OR_CARD_ID>”
 module.exports = fvalues;
-//0000010010321500
-//0000000000000500
