16a762421016dc33642a31f95e639349d69a79be
vikash <krvikash35@gmail.com>
2016-12-24 06:25:04 -0800

    updated

--- web/server/app.js
+++ web/server/app.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 
 global.__proot = __dirname + "/../..";
 app.use('/node_modules', exp.static(__proot + '/node_modules'));
-app.use(exp.static(__proot + '/web/client'));
+app.use('/', exp.static(__proot + '/web/client'));
 
 
 http.listen(3000, function(){
