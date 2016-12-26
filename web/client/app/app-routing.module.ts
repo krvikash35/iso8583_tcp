70bbfc1b627a0f04c46df6125c528c4facbd85ec
vikash <krvikash35@gmail.com>
2016-12-26 17:29:28 +0530

    updated

--- web/client/app/app-routing.module.ts
+++ web/client/app/app-routing.module.ts
@@ -8,12 +8,13 @@ import { LogComponent } from './log/log.component';
 import { HelpComponent } from './help/help.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/app/data', pathMatch: 'full' },
+  { path: '', redirectTo: '/app/data', pathMatch: 'full' },  
   { path: 'app/data',  component: DataComponent },
   { path: 'app/config',  component: ConfigComponent },
   { path: 'app/field',  component: FieldComponent },
   { path: 'app/log',  component: LogComponent },
-  { path: 'app/help',  component: HelpComponent }
+  { path: 'app/help',  component: HelpComponent },
+  { path: '**', redirectTo: '/app/data', pathMatch: 'full' },
 ];
 
 @NgModule({
