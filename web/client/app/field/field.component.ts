d7befaf3a50f43164474c40d9e5c9dae6fe61ab9
vikash <krvikash35@gmail.com>
2017-01-05 19:29:53 +0530

    updated

--- web/client/app/field/field.component.ts
+++ web/client/app/field/field.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core'
 })
 export class FieldComponent{
 
+  exportFieldDefinition(): any {
+      this.logService.logEvent("DataComponent.exportReqData...export request data")
+      if (this.reqData && this.reqData.length > 0) {
+          let reqDataObj = this.dataService.cnvrtReqDataArrayToObj(this.reqData)
+          let url = 'data:text/json;charset=utf8,' + encodeURIComponent(JSON.stringify(reqDataObj));
+          return this.sanitizer.bypassSecurityTrustUrl(url);
+      }
+  }
+
+  importFieldDefinition(event: any): any {
+      this.logService.logEvent("DataComponent.importReqData...import request data")
+      let target = event.target || event.srcElement || event.currentTarget;
+      let fileList = target.files;
+      let file = fileList[0];
+      var that = this;
+      target.value = '';
+      this.logService.logInfo("DataComponent:importReqData:fileList ", fileList);
+      let freader = new FileReader();
+      freader.onload = function(event: any) {
+          let fileContent = JSON.parse(event.target.result)
+          that.logService.logInfo("DataComponent:importReqData:fileContent ", fileContent);
+          that.reqData = that.dataService.cnvrtReqDataObjToArray(fileContent);
+          that.dataService.writeToLocalStorage("reqData", that.reqData);
+          that.logService.logEvent("DataComponent.importReqData...updated cache!")
+      }
+      freader.readAsText(file);
+  }
+
+  importUsingCDCI(): any{
+    
+  }
+
 }
