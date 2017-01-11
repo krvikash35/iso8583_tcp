a41cd71c80af9d5e731f4c1a3168e748476f6ec9
vikash <krvikash35@gmail.com>
2017-01-11 10:21:53 +0530

    updated

--- web/test_server/app.js
+++ web/test_server/app.js
@@ -7,7 +7,8 @@ net.createServer(function(sock) {
     console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
     sock.on('data', function(data) {
       var buffer_data = Buffer.from(data);
-      console.log("BYTE RECEVIED FROM CLIENT: %d",buffer_data.length);
+      print_bin_asci_msg(buffer_data)
+      // console.log("BYTE RECEVIED FROM CLIENT: %d",buffer_data.length);
       setTimeout( function(){
         if(clientClosedConn){
           console.log("CLIENT ALREADY CLOSED CONNECTION...DATA LOST");
@@ -27,4 +28,48 @@ net.createServer(function(sock) {
     });
 
 }).listen(PORT, HOST);
-console.log('Server listening on ' + HOST + ':' + PORT);
+console.log('TCP Server listening on ' + HOST + ':' + PORT);
+
+
+function print_bin_asci_msg(buf){
+  console.log("Total " + buf.length + " Bytes Recived" +"..\nBinary Data...");
+  var data_bin = ''
+  var temp;
+  for(var i=0; i<buf.length; i++){
+    temp = buf.toString('hex', i, i+1)
+    data_bin = data_bin + pad(temp, 3, 'r', ' ');
+  }
+  console.log(data_bin);
+  console.log("Ascii Data...");
+  var data_ascii = ''
+  var temp;
+  for(var i=0; i<buf.length; i++){
+    temp = buf.toString('ascii', i, i+1)
+    data_ascii = data_ascii + pad(temp, 3, 'r', ' ');
+  }
+  console.log(data_ascii);
+}
+
+function pad(data, max, rorl, char){
+  var datastr = data.toString();
+  var datalen = datastr.length;
+  if(datalen > max){
+    return datastr.substring(0,max);
+  }
+  if(!char){
+    char = ' ';
+  }
+  if(!rorl){
+    rorl = 'r';
+  }
+  var result = ""
+  for( var i=0; i<max-datalen; i++ ){
+    result = result+char
+  }
+  if(rorl == 'r'){
+    result = data.toString().concat(result)
+  }else {
+    result = result.concat(data.toString());
+  }
+  return result;
+}
